51f22482fb53271477a240042c14da184e0633d1
fadyhub <PUT_YOUR_EMAIL_HERE>
2024-11-08 18:07:07 +0200

    Your commit message here

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
-import '../sass/style.scss';
 import '@fortawesome/fontawesome-free/js/all.min';
+import '../sass/style.scss';
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
